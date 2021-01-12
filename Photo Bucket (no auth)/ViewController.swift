4f755f9c194fec5694e5cfd20933ebd61285da6f
Jacob Hendrich <hendrijm@rose-hulman.edu>
2021-01-12 12:19:57 -0500

    Initial Commit

---
+++ Photo Bucket (no auth)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photo Bucket (no auth)
+//
+//  Created by Jacob Hendrich on 1/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
